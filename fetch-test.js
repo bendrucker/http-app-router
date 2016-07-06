b02b438bc187054eaa35d56afc51efdad69a889c
Ben Drucker <bvdrucker@gmail.com>
2016-07-05 22:23:59 -0700

    Replace noop w/ t.pass to assert call

--- fetch-test.js
+++ fetch-test.js
@@ -4,7 +4,7 @@ const test = require('tape')
 const proxyquire = require('proxyquire')
 
 test('fetch', function (t) {
-  t.plan(3)
+  t.plan(4)
 
   const fetch = proxyquire('./fetch', {
     'simple-get': function (options, callback) {
@@ -14,10 +14,11 @@ test('fetch', function (t) {
           foo: 'bar'
         }
       })
+      callback()
     }
   })
 
   t.throws(fetch, /app is required/)
   t.throws(fetch.bind(null, {}), /path is required/)
-  fetch({host: 'host.co', headers: {foo: 'bar'}}, '/path?page=2', function noop () {})
+  fetch({host: 'host.co', headers: {foo: 'bar'}}, '/path?page=2', t.pass)
 })
