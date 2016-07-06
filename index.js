9bc7f19878c2f6c828a61e70fbd30e295f087bea
Ben Drucker <bvdrucker@gmail.com>
2016-07-05 19:44:03 -0700

    mv: router.js index.js

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 'use strict'
+
+const HashRouter = require('http-hash-router')
+const partial = require('ap').partial
+const validate = require('./validate')
+const fetch = require('./fetch')
+const Transform = require('./transform')
+
+module.exports = AppRouter
+
+function AppRouter (apps) {
+  var _default = null
+  const router = HashRouter()
+
+  validate(apps)
+  apps.forEach(createRoutes)
+
+  return function (req, res, callback) {
+    router(req, res, {}, function (err) {
+      if (err.type === 'http-hash-router.not-found' && _default) {
+        return _default(req, res, callback)
+      }
+
+      callback(err)
+    })
+  }
+
+  function createRoutes (app) {
+    if (app.routes === '*') {
+      _default = partial(sendApp, app)
+      return
+    }
+
+    app.routes.forEach((route) => router.set(route, partial(sendApp, app)))
+  }
+}
+
+function sendApp (app, req, res, opts, callback) {
+  fetch(app, {path: req.path}, function (err, html) {
+    if (err) return callback(err)
+    res.statusCode = html.statusCode
+
+    html
+      .pipe(Transform(app))
+      .pipe(res)
+  })
+}
