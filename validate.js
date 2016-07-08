99e28ec9adcb89a7215a90f7e4a986c755b8d4c5
Ben Drucker <bvdrucker@gmail.com>
2016-07-08 14:14:23 -0700

    schema: switch to ajv for compilation/error gen

--- validate.js
+++ validate.js
@@ -1,23 +1,17 @@
 'use strict'
 
 const assert = require('assert')
-const Validator = require('is-my-json-valid')
 const transforms = require('./transforms')
-const schema = require('./schema.json')
-
-const schemaValidator = Validator(schema)
+const schema = require('./schemas')
 
 module.exports = validate
 
 function validate (apps) {
+  const valid = schema({apps: apps})
+  if (!valid) throw new Error(schema.errors[0].message)
+
   const wilcards = apps.filter((app) => app.routes === '*')
   assert(wilcards.length <= 1, 'multiple wildcard apps are not allowed')
-
-  apps.forEach(function (app) {
-    const valid = schemaValidator(app)
-    const errors = schemaValidator.errors || [{}]
-    assert(valid, `"${app.name}" is invalid: ${errors[0].field} ${errors[0].message}`)
-  })
 
   apps
     .reduce((acc, app) => acc.concat(app.transforms || []), [])
--- validation-test.js
+++ validation-test.js
@@ -7,20 +7,20 @@ test('validate', function (t) {
   t.test('schema', function (t) {
     t.throws(validate.bind(null, [
       {}
-    ]), /name is required/, 'requires name')
+    ]), /should have required property 'name'/, 'requires name')
 
     t.throws(validate.bind(null, [
       {
         name: 'foo'
       }
-    ]), /routes is required/, 'requires routes')
+    ]), /should have required property 'routes'/, 'requires routes')
 
     t.throws(validate.bind(null, [
       {
         name: 'foo',
         routes: '*'
       }
-    ]), /host is required/, 'requires host')
+    ]), /should have required property 'host'/, 'requires host')
 
     t.end()
   })
